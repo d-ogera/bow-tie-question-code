9de1806049bfe3bf52d3d11463f5ccb49a9fe96b
Ogera <107644200+d-ogera@users.noreply.github.com>
2024-08-17 15:15:34 +0300

    modified css to add mediaqueries

--- user.php
+++ user.php
@@ -1,51 +1,10 @@
-<?php
-require "conn.php";
-
-// Example question ID (this could be passed dynamically through a GET parameter)
-$question_id = 4;
-
-// Fetch question details
-$question_query = "SELECT * FROM quizes WHERE question_id = $question_id";
-$question_result = $conn->query($question_query);
-$question = $question_result->fetch_assoc();
-$allsubcats = mysqli_num_rows($question_result);
-
-// Fetch actions (both correct and incorrect)
-$actions_query = "
-    SELECT action_id, action_text, qa_is_correct
-    FROM actions 
-    INNER JOIN question_actions ON action_id = qa_action
-    WHERE qa_question = $question_id";
-$actions_result = $conn->query($actions_query);
-
-
-
-// Fetch conditions (both correct and incorrect)
-$conditions_query = "
-    SELECT condition_id, condition_text, qc_is_correct
-    FROM conditions
-    INNER JOIN question_conditions ON condition_id = qc_condition
-    WHERE qc_question = $question_id";
-$conditions_result = $conn->query($conditions_query);
-
-// Fetch parameters (both correct and incorrect)
-$parameters_query = "
-    SELECT parameter_id, parameter_text, qp_is_correct
-    FROM parameters
-    INNER JOIN question_parameters ON parameter_id = qp_parameter
-    WHERE qp_question = $question_id";
-$parameters_result = $conn->query($parameters_query);
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bow-Tie Question </title>
+    <title>Bow-Tie Question</title>
     <link href="styles.css" rel="stylesheet">
-
 </head>
 <body>
 
@@ -87,8 +46,7 @@ $parameters_result = $conn->query($parameters_query);
 
     <h3>Options</h3>
     <div class="container">
-
-                <!-- Available Actions -->
+        <!-- Available Actions -->
         <div class="box">
             <h4>Actions</h4>
             <?php while($action = $actions_result->fetch_assoc()): ?>
